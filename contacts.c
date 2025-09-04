fff87d3f56a5a520761959af6a19f39a44800b03
Achraf-podcast <78278926+Achraf-podcast@users.noreply.github.com>
2025-09-04 17:28:53 +0100

    Add files via upload

---
+++ contacts.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+
+struct Livre {
+    char titre[100];
+    char auteur[100];
+    int annee;
+};
+
+
+struct Livre creerLivre(char titre[], char auteur[], int annee) {
+    struct Livre livre;
+    strcpy(livre.titre, titre);
+    strcpy(livre.auteur, auteur);
+    livre.annee = annee;
+    return livre;
+}
+
+int main() {
+    struct Livre monLivre = creerLivre("boite", "ahmed", 1952);
+    struct Livre monLivre2 = creerLivre("antigone", "achraf", 1964);
+    
+    printf("Titre: %s\n", monLivre.titre);
+    printf("Auteur: %s\n", monLivre.auteur);
+    printf("Annee: %d\n", monLivre.annee);
+    
+    return 0;
+}
